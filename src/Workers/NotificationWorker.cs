2a2ace894f4b36e0d9575555ccdca5010e005e6b
Caio Santos <caiosantos.dev@outlook.com>
2026-03-19 14:49:59 -0300

    fix

--- src/Workers/NotificationWorker.cs
+++ src/Workers/NotificationWorker.cs
@@ -13,7 +13,7 @@ public class NotificationWorker(IServiceProvider serviceProvider, ILogger<Notifi
 
         while (!stoppingToken.IsCancellationRequested)
         {
-            wait ProcessPendingJobsAsync();
+            await ProcessPendingJobsAsync();
             await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
         }
     }
